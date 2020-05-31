c3d3687cfe965447e93dcb2ac4001af87cad9d6f
kalyan <kalyanveenam@gmail.com>
2020-05-31 17:12:06 +0530

    Final code- Added step definitions and pages

--- brighttalktech-automation-test-task-maven/src/main/java/AutomationTest/BrightTalkTest/HTTPMethods.java
+++ brighttalktech-automation-test-task-maven/src/main/java/AutomationTest/BrightTalkTest/HTTPMethods.java
@@ -1,68 +1,111 @@
 package AutomationTest.BrightTalkTest;
+
 import io.restassured.RestAssured;
-import io.restassured.RestAssured.*;
 import io.restassured.http.ContentType;
-import io.restassured.matcher.RestAssuredMatchers.*;
-import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import io.restassured.response.ResponseBody;
 import io.restassured.specification.RequestSpecification;
 
-import org.hamcrest.Matchers.*;
-import org.testng.Assert;
+import org.json.JSONException;
 
-import static io.restassured.RestAssured.*;        
-        
+import static io.restassured.RestAssured.*;
+
 public class HTTPMethods {
+	static String Resp;
+	static String loginResponse;
+	static String request_body;
 
-	public static void getResponseBodyFromBaseurl(String baseUrl){
+// input: baseURL
+// functionality: HTTP GET call and converts response to String
+// return type: void
+	public static void getResponseBodyFromBaseurl(String baseUrl) {
 		RequestSpecification httpRequest = RestAssured.given();
-		 Response response = httpRequest.get(baseUrl);
-		 ResponseBody body = response.getBody();
-		 System.out.println("Response Body is: " + body.asString());
-	
-	
+		Response response = httpRequest.get(baseUrl);
+		ResponseBody body = response.getBody();
+		System.out.println("Response Body is: " + body.asString());
+
+	}
+
+// This method returns status code as return type 
+	public static int getResponseStatus(String baseUrl) {
+		int statusCode = given().when().get(baseUrl).getStatusCode();
+		System.out.println("The response status is " + statusCode);
+		return statusCode;
+
+	}
+
+//This method will take a key value as argument and returns true if key element
+//is present in response	
+	public static boolean isResponseContainsKey(String baseUrl, String key) {
+		RestAssured.baseURI = baseUrl;
+		RequestSpecification httpRequest = RestAssured.given();
+		Response response = httpRequest.get(baseUrl);
+		ResponseBody body = response.getBody();
+
+		String bodyAsString = body.asString();
+		System.out.println("Body of a String is " + bodyAsString);
+		return bodyAsString.contains(key);
+	}
+	// This method will return String from response
+
+	public static String getJsonResponseAsString(String baseUrl) {
+		RestAssured.baseURI = baseUrl;
+		RequestSpecification httpRequest = RestAssured.given();
+		Response response = httpRequest.get(baseUrl);
+		if (response.statusCode() == 200) {
+			System.out.println("User found");
+			ResponseBody body = response.getBody();
+
+			String bodyAsString = body.asString();
+			System.out.println("RESPONSE IN STRING " + bodyAsString);
+			return bodyAsString;
+
+		} else {
+			System.out.println("User not found");
+			return "User not found";
 		}
-	
-	public static int getResponseStatus(String baseUrl){
-		   int statusCode= given().when().get(baseUrl).getStatusCode();
-		   System.out.println("The response status is "+statusCode);
-           return statusCode;
-           
 
-		}
-	public static void getPathValue(String baseUrl) {
-
-
-String user_Id =given().when().get(baseUrl).then().extract().path("data");
-System.out.println(user_Id);	
 	}
-	public static  boolean isResponseContainsKey(String baseUrl, String key)
-	{
-		 RestAssured.baseURI = baseUrl;
-		 RequestSpecification httpRequest = RestAssured.given();
-		 Response response = httpRequest.get(baseUrl);
-		 ResponseBody body = response.getBody();
 
-		 String bodyAsString = body.asString();
-		 System.out.println("Body of a String is "+bodyAsString);
-	return bodyAsString.contains(key);	
+	// This reusable method allows users to do POST Operation by providing required
+	// fields
+	public static void POSTRequest(String baseUrl, String name, String job) {
+
+		request_body = " '   {  '  + \r\n" + " '       \"name\": \"" + name + "+\",  '  + \r\n"
+				+ " '       \"job\": \"\"" + job + "  '  + \r\n" + " '  }  ' ; ";
+
+		RestAssured.baseURI = baseUrl;
+
+		  given().body(request_body).when().post("/api/users").then().assertThat().statusCode(201)
+				.and().contentType(ContentType.JSON).and().header("server", "cloudflare").and()
+				.header("content-length", "51").extract().response().asString();
+
+		System.out.println("Response is\t" + Resp);
+
 	}
-	public static  String getJsonResponseAsString(String baseUrl)
-	{
-		 RestAssured.baseURI = baseUrl;
-		 RequestSpecification httpRequest = RestAssured.given();
-		 Response response = httpRequest.get(baseUrl);
-		 ResponseBody body = response.getBody();
 
-		 String bodyAsString = body.asString();
-		 System.out.println("RESPONSE IN STRING "+bodyAsString);
-		 return bodyAsString;
+//POST call for login Functionality
+	public static void getResponsePostCall() throws JSONException {
 
-	  
-		 
-		 
+		Resp = given().body("bo").when().post("/users").then().assertThat().statusCode(201).and()
+				.contentType(ContentType.JSON).and().header("server", "cloudflare").and().header("content-length", "51")
+				.extract().response().asString();
+		System.out.println(Resp);
+
 	}
-	
-	
+
+//Validating the response 
+	public static void validateResponse(String baseUrl, String email, String password) {
+		String bo = " '   {  '  + \r\n" + " '       \"email\": \"eve.holt@reqres.in\",  '  + \r\n"
+				+ " '       \"password\": \"cityslicka\"  '  + \r\n" + " '  }  ' ; ";
+
+		RestAssured.baseURI = "https://reqres.in";
+
+		String Resp = given().body(bo).when().post("/api/login").then().assertThat().statusCode(400).and()
+				.contentType(ContentType.JSON).and().header("server", "cloudflare").and().header("content-length", "37")
+				.extract().response().asString();
+
+		System.out.println("Response is\t" + Resp);
+	}
+
 }
--- brighttalktech-automation-test-task-maven/src/test/java/AutomationTest/BrightTalkTest/StepDefinition.java
+++ brighttalktech-automation-test-task-maven/src/test/java/AutomationTest/BrightTalkTest/StepDefinition.java
@@ -1,71 +1,147 @@
 package AutomationTest.BrightTalkTest;
 
+import cucumber.api.DataTable;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
-import gherkin.deps.com.google.gson.Gson;
-import gherkin.deps.com.google.gson.JsonElement;
-import gherkin.deps.com.google.gson.JsonObject;
-
 import static AutomationTest.BrightTalkTest.HomePage.homePage;
-
+import java.util.List;
 import org.json.JSONException;
 import org.json.JSONObject;
-import org.testng.Assert;
-
 import static AutomationTest.BrightTalkTest.HTTPMethods.getResponseStatus;
-import static AutomationTest.BrightTalkTest.HTTPMethods.getResponseBodyFromBaseurl;
-import static AutomationTest.BrightTalkTest.HTTPMethods.getPathValue;
 import static AutomationTest.BrightTalkTest.HTTPMethods.isResponseContainsKey;
 import static AutomationTest.BrightTalkTest.HTTPMethods.getJsonResponseAsString;
-
+import static AutomationTest.BrightTalkTest.HTTPMethods.getResponsePostCall;
+import static AutomationTest.BrightTalkTest.HTTPMethods.validateResponse;
+import static AutomationTest.BrightTalkTest.HTTPMethods.POSTRequest;
 
 public class StepDefinition {
-	
-	String baseUrl="https://reqres.in/api";
- 
-    @Given("^I am on the home page$")
-    public void iAmOnTheHomePage() {
-    	
-           homePage();
-    }
-    @Given("^I get the default list of users for on 1st page$")
-    public void getDefaultListOfUsers() throws JSONException {
-    	String respose=getJsonResponseAsString(baseUrl+"/users?page=1");
-    
-    	
-    	
-         // convert to json object
-    	  JSONObject json = new JSONObject(respose);
-         // print object
-    	 json.get("per_page").toString();
-         System.out.println("TEST"+json.get("per_page"));
-         // get value for a key
- 
-	
-    
-    }
-    @When ("^I get the list of all users$")
-    public void getFirstPageListOfUsers() {
-    	int response=getResponseStatus(baseUrl+"/users?page=1");	
-    	if(response==200) {
-    		System.out.println("Defualt list of user are getting displayed");
-    		
-    	}
+	// declaring global variables
+	String baseUrl = "https://reqres.in/api";
+	String getUsersPaginationPath = "/users?page=1";
+	String invalidUserPath = "/user/55";
 
-    	}
-    @Then("^I should see total users count equals to number of user ids$")
-    public void verifyUserCountEqualsuserId() {
-    	boolean isFound=isResponseContainsKey(baseUrl+"/users?page=1","George");
-    	
+	@Given("^I am on the home page$")
+	public void iAmOnTheHomePage() {
 
-    	if(isFound) {
-    		System.out.println("Total User count is equal to number of user IDs");
-    	}
-    	else {
-    		System.out.println("Total User count is not equal to number of user IDs");
-    	}
-    	
-    }
+		homePage();
+	}
+
+	// displaying list of users received from server
+	@Given("^I get the default list of users for on 1st page$")
+	public void getDefaultListOfUsers() throws JSONException {
+		String response = getJsonResponseAsString(baseUrl + getUsersPaginationPath);
+		System.out.println("Defualt list of user are getting displayed");
+		System.out.println(response);
+
+	}
+
+	// verifying the status code , if it is 200OK, verifying for a key element
+	@When("^I get the list of all users$")
+	public void getFirstPageListOfUsers() {
+		int response = getResponseStatus(baseUrl + getUsersPaginationPath);
+		if (response == 200) {
+			boolean isFound = isResponseContainsKey(baseUrl + getUsersPaginationPath, "first_name");
+			if (isFound) {
+				System.out.println("Server responded with 200OK, List of all users are getting displayed");
+			} else {
+				System.out.println("Server responded with Error status code" + response);
+			}
+
+		}
+
+	}
+
+	// converting string to JSONObject
+	// As the response has paginated data, marking case as pass when
+	// total count=length of user array* number of pages
+	@Then("^I should see total users count equals to number of user ids$")
+	public void verifyUserCountEqualsuserId() throws JSONException {
+
+		String response = getJsonResponseAsString(baseUrl + getUsersPaginationPath);
+		JSONObject json = new JSONObject(response);
+		String expectedUserCount = json.get("total").toString();
+
+		int userPage1 = json.getJSONArray("data").length();
+		String totalPages = json.get("total_pages").toString();
+		int totalPagesInt = Integer.parseInt(totalPages);
+
+		int UserCountInt = totalPagesInt * userPage1;
+		String actualUserCount = String.valueOf(UserCountInt);
+
+		System.out.print("Expected:" + expectedUserCount);
+		System.out.print("Actual:" + actualUserCount);
+
+		if (expectedUserCount.equalsIgnoreCase(actualUserCount)) {
+			System.out.println("Total user count is equal to no of user ID's");
+		} else {
+			System.out.println("Total user count is not equal to no of user ID's");
+		}
+	}
+
+//searching for an object which is not present 
+	@Given("^I make a search for user 55$")
+	public void searchUser() throws JSONException {
+
+		getJsonResponseAsString(baseUrl + invalidUserPath);
+	}
+
+//validating error
+	@Then("^I receive error code in response$")
+	public void validateErrorCode() {
+
+		String response = getJsonResponseAsString(baseUrl + invalidUserPath);
+		if (response.equals("User not found")) {
+			System.out.println("User not found");
+		}
+	}
+
+//creating a user, handled in HTTPMethods
+	@Given("I create user with following (.*) (.*)")
+	public void createUserWithData(String name, String job) throws JSONException {
+		POSTRequest(baseUrl, name, job);
+	}
+
+//verifying the response after posting the data
+	@Then("response should contain folowing data")
+	public void verifyUser() throws JSONException {
+		getResponsePostCall();
+	}
+
+//Validating login functionality, using data table to pass data
+	@Given("^I login succesfully with following data$")
+	public void loginVerify(DataTable dt) {
+		List<String> list = dt.asList(String.class);
+		System.out.println("Username - " + list.get(0));
+		System.out.println("Password - " + list.get(1));
+
+		validateResponse(baseUrl, list.get(0), list.get(1));
+
+	}
+
+//verify user is present after loading a page for some time
+	@Given("^I wait for user list to load$")
+	public void waitForUserListToLoad() {
+		isResponseContainsKey(baseUrl + getUsersPaginationPath, "user");
+	}
+
+//verifying each ID is unique 
+	@Then("^I should see that every user has a unique id$")
+	public void uniqueIdVerify() throws JSONException {
+
+		String response = getJsonResponseAsString(baseUrl + getUsersPaginationPath);
+		JSONObject json = new JSONObject(response);
+		int length = json.getJSONArray("data").length();
+		System.out.println(json.getJSONArray("data").get(2).toString().substring(1, 20));
+		for (int i = 0; i < length - 1; i++) {
+
+			if (json.getJSONArray("data").get(i).toString().substring(1, 20)
+					.equals(json.getJSONArray("data").get(i + 1).toString().substring(1, 20))) {
+				System.out.print("ID is not unique");
+			} else {
+				System.out.print("ID is  unique");
+			}
+		}
+	}
+
 }
-
